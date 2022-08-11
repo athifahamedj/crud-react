5453459772ef9581e204fb5b258ffb0719aaf264
Athif Ahamed <81356981+athifahamedj@users.noreply.github.com>
2022-08-11 12:15:34 +0530

    added a date

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const dummyDatas = [
-	// {
-	// 	id: "e1",
-	// 	title: "Toilet Paper",
-	// 	amount: 94.12,
-	// 	date: new Date(2020, 7, 14),
-	// },
+	{
+		id: "e1",
+		title: "A book",
+		amount: 94.12,
+		date: new Date(2020, 7, 14),
+	},
 	// { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
 	// {
 	// 	id: "e3",
